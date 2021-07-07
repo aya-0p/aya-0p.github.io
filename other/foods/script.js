53738bb06c8b7a9d30a402d6f1bdef6d3d390829
aya-0p <69342748+aya-0p@users.noreply.github.com>
2021-07-07 11:35:52 +0900

    Update script.js

--- other/foods/script.js
+++ other/foods/script.js
@@ -1,4 +1,4 @@
 function main(){}
 fetch("https://aya-0p.github.io/other/foods/crafts.json")
-  .then(response => JSON.parse(response)
+  .then(response => JSON.parse(response))
   .then(data => document.getElementById("1").innerHTML = data);
