ed319152b69bb3d53e75d186e2c1da25c3d23067
aya-0p <69342748+aya-0p@users.noreply.github.com>
2021-07-07 13:35:41 +0900

    Add files via upload

--- other/foods/script.js
+++ other/foods/script.js
@@ -1,19 +1,19 @@
 let oReq,
     craft,
-    num = "a",
+    num = 1,
     lang = "Japanese";
 function main() {
   getJson();
   document.getElementById("name").innerHTML = craft.num.lang;
   document.getElementById("recovery").innerHTML = craft.num.Recovery;
   document.getElementById("amount").innerHTML = carft.num.Amount;
-  document.grtElementById("size").innerHTML = craft.num.Size;
+  document.getElementById("size").innerHTML = craft.num.Size;
   document.getElementById("other").innerHTML = carft.num.Other;
 }
 function reqListener() {
   //document.getElementById("1").innerHTML = this.responseText;
-  console.log(this.responseText);
-  craft = this.responseText;
+  console.log(JSON.parse(this.responseText));
+  craft = JSON.parse(this.responseText);
 }
 function getJson() {
   oReq = new XMLHttpRequest();
