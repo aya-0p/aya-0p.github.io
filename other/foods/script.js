5f8c7f00953709e566c2ea0126e1e2bf15bee671
aya-0p <69342748+aya-0p@users.noreply.github.com>
2021-07-08 09:06:14 +0900

    Add files via upload

--- other/foods/script.js
+++ other/foods/script.js
@@ -1,24 +1,13 @@
-let oReq,
-    craft,
+let oReq;
+var craft,
     num = "a",
     lang = "Japanese";
-async function main() {
-  await getJson();
-  document.getElementById("name").innerHTML = craft[num][lang];
-  document.getElementById("recovery").innerHTML = craft[num]["Recovery"];
-  document.getElementById("amount").innerHTML = craft[num]["Amount"];
-  document.getElementById("size").innerHTML = craft[num]["Size"];
-  document.getElementById("other").innerHTML = craft[num]["Other"];
-}
-function reqListener() {
-  //document.getElementById("1").innerHTML = this.responseText;
-  console.log(JSON.parse(this.responseText));
-  craft = JSON.parse(this.responseText);
-}
-function getJson() {
-  oReq = new XMLHttpRequest();
-  oReq.addEventListener("load", reqListener);
-  oReq.open("GET", "https://aya-0p.github.io/other/foods/crafts.json");
-  oReq.send();
-}
-main()
+oReq = new XMLHttpRequest();
+oReq.addEventListener("load", function{craft = JSON.parse(this.responseText)});
+oReq.open("GET", "https://aya-0p.github.io/other/foods/crafts.json");
+oReq.send();
+document.getElementById("name").innerHTML = craft[num][lang];
+document.getElementById("recovery").innerHTML = craft[num]["Recovery"];
+document.getElementById("amount").innerHTML = craft[num]["Amount"];
+document.getElementById("size").innerHTML = craft[num]["Size"];
+document.getElementById("other").innerHTML = craft[num]["Other"];
