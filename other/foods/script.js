2d191086a8c5fbac1e38db2825e7535b08cad947
aya-0p <69342748+aya-0p@users.noreply.github.com>
2021-07-08 11:44:47 +0900

    Update script.js

--- other/foods/script.js
+++ other/foods/script.js
@@ -1,6 +1,1 @@
-let oReq;
-var craft;
-oReq = new XMLHttpRequest();
-oReq.addEventListener("load", function(){craft = JSON.parse(this.responseText)});
-oReq.open("GET", "https://aya-0p.github.io/other/foods/crafts.json");
-oReq.send();
+
