37a529b9683d32c1e4de3601ab0ebb242b052126
aya-0p <69342748+aya-0p@users.noreply.github.com>
2021-07-10 09:28:14 +0900

    Update share.js

--- other/share.js
+++ other/share.js
@@ -1,6 +1,6 @@
 (document.getElementsByTagName("body"))[0].innerHTML = '<div id="b1"></div><br><div id=b2></div><div id="b3"></div>';
 document.getElementById("b1").innerHTML = '<p>以下から選んでください</p><form action="#" onsubmit="return getData()"><label><button type="submit">受け取る</button></label></form><br>';
-document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()" name="nam"><label><select name="opts" size="4"><optgroup label="共有する--ワールドを以下から選択" id="share"></optgroup></select><br><button type="submit" id="sendData">決定</button></label></form><br>'
+document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()" name="nam"><label><select name="opts" size="4"><optgroup label="共有する--ワールドを以下から選択" id="share"></optgroup></select><br><button type="submit">決定</button></label></form><br>'
 document.getElementById("b3").innerHTML = '<form action="#"><label><button type="submit" id="rev">元の画面に戻る</button></label></form>'
 var dList = JSON.parse(localStorage.getItem("document_list"));
 var ls = dList.splice(1,dList.length - 1);
@@ -36,7 +36,6 @@ async function postData() {
     return false;
 }
 async function postData2(e) {
-    window.alert(e);
     var xl = new XMLHttpRequest();
     xl.open("POST", "https://script.google.com/macros/s/AKfycbz1wcN8hmiUfsPOewChFVbMzWH_AQRNxvbHkzaArVALQXK9dTEXyx9iPVP8GJRzf7Ar/exec")
     xl.send("name="+e+"&data="+JSON.parse(localStorage.getItem(e)));
