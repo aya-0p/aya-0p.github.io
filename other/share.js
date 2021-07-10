ae3f103b91233c7bfa417740aa8a7ab65d4e618a
aya-0p <69342748+aya-0p@users.noreply.github.com>
2021-07-10 11:09:23 +0900

    Update share.js

--- other/share.js
+++ other/share.js
@@ -36,15 +36,23 @@ async function postData() {
     return false;
 }
 async function postData2(e) {
-    document.getElementById("b1").innerHTML = "しばらくお待ちください...";
+    document.getElementById("b1").innerHTML = null;
     document.getElementById("b2").innerHTML = null;
-    document.getElementById("b3").innerHTML = null;
+    document.getElementById("b3").innerHTML = '<form action="#"><label><button type="submit" id="rev">元の画面に戻る</button></label></form>';
     var xl = new XMLHttpRequest();
     xl.open("POST", "https://script.google.com/macros/s/AKfycbz1wcN8hmiUfsPOewChFVbMzWH_AQRNxvbHkzaArVALQXK9dTEXyx9iPVP8GJRzf7Ar/exec", false);
     xl.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
     var datas = {name:e, data:JSON.parse(localStorage.getItem(e))};
     xl.send(JSON.stringify(datas));
     var res = xl.response;
-    window.alert(res);
+    if (res == 1) {
+        window.alert("ワールドは上書き保存されました");
+    } else if (res == 0) {
+        window.alert("ワールドは新規に保存されました");
+    } else {
+        console.log("post error");
+        console.log("error id=" + res);
+        window.alert("不明なエラーにより保存されませんでした");
+    }
     return false;
 }
