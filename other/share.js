fee8607bb953c499f71239b40365b8063897f3ea
aya-0p <69342748+aya-0p@users.noreply.github.com>
2021-07-10 12:27:13 +0900

    Update share.js

--- other/share.js
+++ other/share.js
@@ -28,12 +28,24 @@ async function getData2() {
     nw.send();
     await sleep(5000);
     var res = nw.response;
+    if (res == 0) {
+        window.alert("その名前のワールドは存在しません");
+        document.getElementById("b1").innerHTML = '<p>以下から選んでください</p><form action="#" onsubmit="return getData()"><label><button type="submit">受け取る</button></label></form><br>';
+        document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()" name="nam"><label><select name="opts" size="4"><optgroup label="共有する--ワールドを以下から選択" id="share"></optgroup></select><br><button type="submit">決定</button></label></form><br>'
+        document.getElementById("b3").innerHTML = '<form action="#"><label><button type="submit" id="rev">元の画面に戻る</button></label></form>'
+        var dList = JSON.parse(localStorage.getItem("document_list"));
+        var ls = dList.splice(1,dList.length - 1);
+for (var i = 0; i < ls.length; i++) {
+    document.getElementById("share").innerHTML += `<option value="` + ls[i] + `">` + ls[i] + `</option>`;
+}
+    } else {
     var wN = LoadProc();
     localStorage.setItem(wN,res);//JSON.stringify(res)
     var n = JSON.parse(localStorage.getItem("document_list"));
     n.push(wN);
     localStorage.setItem("document_list",JSON.stringify(n));
     //window.alert(res);
+    }
     return false;
 }
 async function postData() {
@@ -42,9 +54,9 @@ async function postData() {
 }
 async function postData2(e) {
     var wName = window.prompt("保存する名前を入力してください\n注意：よく使われそうな名前は使わないでください、ほかの人から上書き保存される可能性があります");
-    document.getElementById("b1").innerHTML = null;
+    document.getElementById("b1").innerHTML = "しばらくお待ちください...";
     document.getElementById("b2").innerHTML = null;
-    document.getElementById("b3").innerHTML = '<form action="#"><label><button type="submit" id="rev">元の画面に戻る</button></label></form>';
+    document.getElementById("b3").innerHTML = null;
     var xl = new XMLHttpRequest();
     xl.open("POST", "https://script.google.com/macros/s/AKfycbz1wcN8hmiUfsPOewChFVbMzWH_AQRNxvbHkzaArVALQXK9dTEXyx9iPVP8GJRzf7Ar/exec", false);
     xl.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
@@ -59,6 +71,14 @@ async function postData2(e) {
         console.log("post error");
         console.log("error id=" + res);
         window.alert("不明なエラーにより保存されませんでした");
+    }
+    document.getElementById("b1").innerHTML = '<p>以下から選んでください</p><form action="#" onsubmit="return getData()"><label><button type="submit">受け取る</button></label></form><br>';
+    document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()" name="nam"><label><select name="opts" size="4"><optgroup label="共有する--ワールドを以下から選択" id="share"></optgroup></select><br><button type="submit">決定</button></label></form><br>'
+    document.getElementById("b3").innerHTML = '<form action="#"><label><button type="submit" id="rev">元の画面に戻る</button></label></form>'
+    var dList = JSON.parse(localStorage.getItem("document_list"));
+    var ls = dList.splice(1,dList.length - 1);
+    for (var i = 0; i < ls.length; i++) {
+        document.getElementById("share").innerHTML += `<option value="` + ls[i] + `">` + ls[i] + `</option>`;
     }
     return false;
 }
