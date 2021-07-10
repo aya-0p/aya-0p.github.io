9e620c08ec5e7e6544fd1e7c34ec9ef1411b0d28
aya-0p <69342748+aya-0p@users.noreply.github.com>
2021-07-10 09:11:53 +0900

    Update share.js

--- other/share.js
+++ other/share.js
@@ -1,7 +1,7 @@
 (document.getElementsByTagName("body"))[0].innerHTML = '<div id="b1"></div><br><div id=b2></div><div id="b3"></div>';
 document.getElementById("b1").innerHTML = '<p>以下から選んでください</p><form action="#" onsubmit="return getData()"><label><button type="submit">受け取る</button></label></form><br>';
-document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()"><label><select name="opts" size="4"><optgroup label="共有する--ワールドを以下から選択" id="share"></optgroup></select><br><button type="submit" id="sendData">決定</button></label></form><br>'
-document.getElementById("b3").innerHTML = '<form action="#"><label><button type="submit">元に戻る</button></label></form>'
+document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()" name="nam"><label><select name="opts" size="4"><optgroup label="共有する--ワールドを以下から選択" id="share"></optgroup></select><br><button type="submit" id="sendData">決定</button></label></form><br>'
+document.getElementById("b3").innerHTML = '<form action="#"><label><button type="submit" id="rev">元の画面に戻る</button></label></form>'
 var dList = JSON.parse(localStorage.getItem("document_list"));
 var ls = dList.splice(1,dList.length - 1);
 for (var i = 0; i < ls.length; i++) {
@@ -18,6 +18,9 @@ async function getData() {
 }
 async function getData2() {
     var worldName = window.prompt("受け取るワールド名を入力してください");
+    document.getElementById("b1").innerHTML = "しばらくお待ちください...";
+    document.getElementById("b2").innerHTML = null;
+    document.getElementById("b3").innerHTML = null;
     var nw = new XMLHttpRequest();
     var url = new URL('https://script.google.com/macros/s/AKfycbz1wcN8hmiUfsPOewChFVbMzWH_AQRNxvbHkzaArVALQXK9dTEXyx9iPVP8GJRzf7Ar/exec');
     url.searchParams.set('name', worldName);
@@ -27,6 +30,10 @@ async function getData2() {
     var res = nw.response;
     window.alert(res);
 }
-function postData() {
+async function postData() {
+    postData2(document.nam.elements[0].value);
     return false;
+}
+async function postData2(e) {
+    window.alert(e);
 }
