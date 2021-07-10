bde65469246f5cbcc8e876577e83b4da4d2e5d86
aya-0p <69342748+aya-0p@users.noreply.github.com>
2021-07-10 23:29:12 +0900

    Update share.js

--- other/share.js
+++ other/share.js
@@ -118,18 +118,22 @@ function del() {
 function delData() {
     console.log(document.nam);
     var delName = document.nam.elements[0].value;
-    document.getElementById("b1").innerHTML = "しばらくお待ちください...";
-    document.getElementById("b2").innerHTML = null;
-    document.getElementById("b3").innerHTML = null;
-    document.getElementById("b4").innerHTML = null;
-    var an = window.prompt("'" + delName + "'を削除します\nよろしければ'ok'と入力してください");
-    if (an == "ok") {
-        var n = JSON.parse(localStorage.getItem("document_list"));
-        var d = n.indexOf(delName);
-        n.splice(d, 1);
-        localStorage.setItem("document_list",JSON.stringify(n));
+    if (delName == null) {
+        window.alert("削除されませんでした");
+    } else {
+        document.getElementById("b1").innerHTML = "しばらくお待ちください...";
+        document.getElementById("b2").innerHTML = null;
+        document.getElementById("b3").innerHTML = null;
+        document.getElementById("b4").innerHTML = null;
+        var an = window.prompt("'" + delName + "'を削除します\nよろしければ'ok'と入力してください");
+        if (an == "ok") {
+            var n = JSON.parse(localStorage.getItem("document_list"));
+            var d = n.indexOf(delName);
+            n.splice(d, 1);
+            localStorage.setItem("document_list",JSON.stringify(n));
+        }
+        window.alert("削除されました");
     }
-    window.alert("削除されました");
     document.getElementById("b1").innerHTML = '<p>以下から選んでください</p><form action="#" onsubmit="return getData()"><label><button type="submit">ダウンロードする</button></label></form><br>';
     document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()" name="nam"><label><select name="opts" size="4"><optgroup label="アップロードする--ワールドを以下から選択" id="share"></optgroup></select><br><button type="submit">決定</button></label></form><br>'
     document.getElementById("b3").innerHTML = '<form action="http://zonest.cn/z/"><label><button type="submit" id="rev">元の画面に戻る</button></label></form>'
