052787ece9a90410b6d0b89d462e54c03eec05a0
aya-0p <69342748+aya-0p@users.noreply.github.com>
2021-07-11 09:47:59 +0900

    Update share.js

--- other/share.js
+++ other/share.js
@@ -1,16 +1,33 @@
-//developer ver 0.1.3
-console.log("developer ver 0.1.3");
+//developer ver 0.1.4
+console.log("developer ver 0.1.4");
 (document.getElementsByTagName("body"))[0].innerHTML = '<div id="b1"></div><br><div id=b2></div><div id="b3"></div><div id="b4"></div><footer><div>Version 0.1</div><style>footer{position: absolute;bottom: 0;}</style></footer>';
-document.getElementById("b1").innerHTML = '<p>以下から選んでください</p><form action="#" onsubmit="return getData()"><label><button type="submit">ダウンロードする</button></label></form><br>';
-document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()" name="nam"><label><select name="opts" id="share"><option value="" selected>アップロードする--ワールドを以下から選択</option></select><br><button type="submit">決定</button></label></form><br>'
-document.getElementById("b3").innerHTML = '<form action="http://zonest.cn/z/"><label><button type="submit" id="rev">元の画面に戻る</button></label></form>'
-document.getElementById("b4").innerHTML = '<br><br><br><br><br><form action="#" onsubmit="return del()"><label><button type="submit">ワールドを強制削除する</button></label></form>'
-var dList = JSON.parse(localStorage.getItem("document_list"));
-var ls = dList.splice(1, dList.length - 1);
-for (var i = 0; i < ls.length; i++) {
-    document.getElementById("share").innerHTML += `<option value="` + ls[i] + `">` + ls[i] + `</option>`;
+function setDefault() {
+    document.getElementById("b1").innerHTML = '<p>以下から選んでください</p><form action="#" onsubmit="return getData()"><label><button type="submit">ダウンロードする</button></label></form><br>';
+    document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()" name="nam"><label><select name="opts" id="share"><option value="" selected>アップロードする--ワールドを以下から選択</option></select><br><button type="submit">決定</button></label></form><br>'
+    document.getElementById("b3").innerHTML = '<form action="http://zonest.cn/z/"><label><button type="submit" id="rev">元の画面に戻る</button></label></form>'
+    document.getElementById("b4").innerHTML = '<br><br><br><br><br><form action="#" onsubmit="return del()"><label><button type="submit">ワールドを強制削除する</button></label></form>'
+    setWorld();
 }
-
+function setWorld() {
+    var dList = JSON.parse(localStorage.getItem("document_list"));
+    var ls = dList.splice(1, dList.length - 1);
+    for (var i = 0; i < ls.length; i++) {
+        document.getElementById("share").innerHTML += `<option value="` + ls[i] + `">` + ls[i] + `</option>`;
+    }
+}
+function plWait() {
+    document.getElementById("b1").innerHTML = "しばらくお待ちください...";
+    document.getElementById("b2").innerHTML = null;
+    document.getElementById("b3").innerHTML = null;
+    document.getElementById("b4").innerHTML = null;
+}
+function setDel() {
+    document.getElementById("b1").innerHTML = "ワールドを削除します。zonest.cn/zが起動しなくなった場合はお使いください";
+        document.getElementById("b2").innerHTML = null;
+        document.getElementById("b4").innerHTML = null;
+        document.getElementById("b3").innerHTML = '<form action="#" onsubmit="return delData()" name="nam"><label><select name="opts2" id="del"><option value="" selected>削除するワールドを以下から選択</option></select><br><button type="submit">削除</button></label></form><br>';
+}
+setDefalut();
 function sleep(msec) {
     return new Promise(function(resolve) {
         setTimeout(function() {
@@ -26,24 +43,10 @@ async function getData2() {
     var worldName = window.prompt("ダウンロードするワールド名を入力してください");
     if (worldName == "" || worldName == null || worldName == undefined) {
         window.alert("中止しました");
-        document.getElementById("b1").innerHTML = null;
-        document.getElementById("b2").innerHTML = null;
-        document.getElementById("b3").innerHTML = null;
-        document.getElementById("b4").innerHTML = null;
-        document.getElementById("b1").innerHTML = '<p>以下から選んでください</p><form action="#" onsubmit="return getData()"><label><button type="submit">ダウンロードする</button></label></form><br>';
-        document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()" name="nam"><label><select name="opts" id="share"><option value="" selected>アップロードする--ワールドを以下から選択</option></select><br><button type="submit">決定</button></label></form><br>'
-        document.getElementById("b3").innerHTML = '<form action="http://zonest.cn/z/"><label><button type="submit" id="rev">元の画面に戻る</button></label></form>'
-        document.getElementById("b4").innerHTML = '<br><br><br><br><br><form action="#" onsubmit="return del()"><label><button type="submit">ワールドを強制削除する</button></label></form>'
-        var dList = JSON.parse(localStorage.getItem("document_list"));
-        var ls = dList.splice(1, dList.length - 1);
-        for (var i = 0; i < ls.length; i++) {
-            document.getElementById("share").innerHTML += `<option value="` + ls[i] + `">` + ls[i] + `</option>`;
-        }
+        plWait();
+        setDefalut();
     } else {
-        document.getElementById("b1").innerHTML = "しばらくお待ちください...";
-        document.getElementById("b2").innerHTML = null;
-        document.getElementById("b3").innerHTML = null;
-        document.getElementById("b4").innerHTML = null;
+        plWait();
         var nw = new XMLHttpRequest();
         var url = new URL('https://script.google.com/macros/s/AKfycbz1wcN8hmiUfsPOewChFVbMzWH_AQRNxvbHkzaArVALQXK9dTEXyx9iPVP8GJRzf7Ar/exec');
         url.searchParams.set('name', worldName);
@@ -53,15 +56,7 @@ async function getData2() {
         var res = nw.response;
         if (res == 0) {
             window.alert("その名前のワールドは存在しません");
-            document.getElementById("b1").innerHTML = '<p>以下から選んでください</p><form action="#" onsubmit="return getData()"><label><button type="submit">ダウンロードする</button></label></form><br>';
-            document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()" name="nam"><label><select name="opts" id="share"><option value="" selected>アップロードする--ワールドを以下から選択</option></select><br><button type="submit">決定</button></label></form><br>'
-            document.getElementById("b3").innerHTML = '<form action="http://zonest.cn/z/"><label><button type="submit" id="rev">元の画面に戻る</button></label></form>'
-            document.getElementById("b4").innerHTML = '<br><br><br><br><br><form action="#" onsubmit="return del()"><label><button type="submit">ワールドを強制削除する</button></label></form>'
-            var dList = JSON.parse(localStorage.getItem("document_list"));
-            var ls = dList.splice(1, dList.length - 1);
-            for (var i = 0; i < ls.length; i++) {
-                document.getElementById("share").innerHTML += `<option value="` + ls[i] + `">` + ls[i] + `</option>`;
-            }
+            setDefalut();
         } else {
             var wN = LoadProc();
             localStorage.setItem(wN, res); //JSON.stringify(res)
@@ -69,15 +64,7 @@ async function getData2() {
             n.push(wN);
             localStorage.setItem("document_list", JSON.stringify(n));
             window.alert("ダウンロードされました\n保存名は保存時の時刻です");
-            document.getElementById("b1").innerHTML = '<p>以下から選んでください</p><form action="#" onsubmit="return getData()"><label><button type="submit">ダウンロードする</button></label></form><br>';
-            document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()" name="nam"><label><select name="opts" id="share"><option value="" selected>アップロードする--ワールドを以下から選択</option></select><br><button type="submit">決定</button></label></form><br>'
-            document.getElementById("b4").innerHTML = '<br><br><br><br><br><form action="#" onsubmit="return del()"><label><button type="submit">ワールドを強制削除する</button></label></form>'
-            document.getElementById("b3").innerHTML = '<form action="http://zonest.cn/z/"><label><button type="submit" id="rev">元の画面に戻る</button></label></form>'
-            var dList = JSON.parse(localStorage.getItem("document_list"));
-            var ls = dList.splice(1, dList.length - 1);
-            for (var i = 0; i < ls.length; i++) {
-                document.getElementById("share").innerHTML += `<option value="` + ls[i] + `">` + ls[i] + `</option>`;
-            }
+            setDefalut();
         }
     }
     return false;
@@ -88,37 +75,15 @@ async function postData() {
 }
 async function postData2(e) {
     if (e == null || e == "" || e == undefined) {
-        document.getElementById("b1").innerHTML = null;
-        document.getElementById("b2").innerHTML = null;
-        document.getElementById("b3").innerHTML = null;
-        document.getElementById("b4").innerHTML = null;
-        document.getElementById("b1").innerHTML = '<p>以下から選んでください</p><form action="#" onsubmit="return getData()"><label><button type="submit">ダウンロードする</button></label></form><br>';
-        document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()" name="nam"><label><select name="opts" id="share"><option value="" selected>アップロードする--ワールドを以下から選択</option></select><br><button type="submit">決定</button></label></form><br>'
-        document.getElementById("b3").innerHTML = '<form action="http://zonest.cn/z/"><label><button type="submit" id="rev">元の画面に戻る</button></label></form>'
-        document.getElementById("b4").innerHTML = '<br><br><br><br><br><form action="#" onsubmit="return del()"><label><button type="submit">ワールドを強制削除する</button></label></form>'
-        var dList = JSON.parse(localStorage.getItem("document_list"));
-        var ls = dList.splice(1, dList.length - 1);
-        for (var i = 0; i < ls.length; i++) {
-            document.getElementById("share").innerHTML += `<option value="` + ls[i] + `">` + ls[i] + `</option>`;
-        }
+        plWait();
+        setDefalut();
     } else {
         var wName = window.prompt("アップロードする名前を入力してください\nこの名前はワールドをダウンロードするときに使います\n注意：よく使われそうな名前は使わないでください、ほかの人から上書き保存される可能性があります");
         if (wName == "" || wName == null || wName == undefined) {
             window.alert("中止しました");
-            document.getElementById("b1").innerHTML = '<p>以下から選んでください</p><form action="#" onsubmit="return getData()"><label><button type="submit">ダウンロードする</button></label></form><br>';
-            document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()" name="nam"><label><select name="opts" id="share"><option value="" selected>アップロードする--ワールドを以下から選択</option></select><br><button type="submit">決定</button></label></form><br>'
-            document.getElementById("b3").innerHTML = '<form action="http://zonest.cn/z/"><label><button type="submit" id="rev">元の画面に戻る</button></label></form>'
-            document.getElementById("b4").innerHTML = '<br><br><br><br><br><form action="#" onsubmit="return del()"><label><button type="submit">ワールドを強制削除する</button></label></form>'
-            var dList = JSON.parse(localStorage.getItem("document_list"));
-            var ls = dList.splice(1, dList.length - 1);
-            for (var i = 0; i < ls.length; i++) {
-                document.getElementById("share").innerHTML += `<option value="` + ls[i] + `">` + ls[i] + `</option>`;
-            }
+            setDefalut();
         } else {
-            document.getElementById("b1").innerHTML = "しばらくお待ちください...";
-            document.getElementById("b2").innerHTML = null;
-            document.getElementById("b3").innerHTML = null;
-            document.getElementById("b4").innerHTML = null;
+            plWait();
             var xl = new XMLHttpRequest();
             xl.open("POST", "https://script.google.com/macros/s/AKfycbz1wcN8hmiUfsPOewChFVbMzWH_AQRNxvbHkzaArVALQXK9dTEXyx9iPVP8GJRzf7Ar/exec", false);
             xl.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
@@ -137,15 +102,7 @@ async function postData2(e) {
                 console.log("error id=" + res);
                 window.alert("不明なエラーによりアップロードされませんでした");
             }
-            document.getElementById("b1").innerHTML = '<p>以下から選んでください</p><form action="#" onsubmit="return getData()"><label><button type="submit">ダウンロードする</button></label></form><br>';
-            document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()" name="nam"><label><select name="opts" id="share"><option value="" selected>アップロードする--ワールドを以下から選択</option></select><br><button type="submit">決定</button></label></form><br>'
-            document.getElementById("b4").innerHTML = '<br><br><br><br><br><form action="#" onsubmit="return del()"><label><button type="submit">ワールドを強制削除する</button></label></form>'
-            document.getElementById("b3").innerHTML = '<form action="http://zonest.cn/z/"><label><button type="submit" id="rev">元の画面に戻る</button></label></form>'
-            var dList = JSON.parse(localStorage.getItem("document_list"));
-            var ls = dList.splice(1, dList.length - 1);
-            for (var i = 0; i < ls.length; i++) {
-                document.getElementById("share").innerHTML += `<option value="` + ls[i] + `">` + ls[i] + `</option>`;
-            }
+            setDefalut();
             return false;
         }
     }
@@ -163,15 +120,8 @@ function LoadProc() {
 }
 
 function del() {
-    document.getElementById("b1").innerHTML = "ワールドを削除します。zonest.cn/zが起動しなくなった場合はお使いください";
-    document.getElementById("b2").innerHTML = null;
-    document.getElementById("b4").innerHTML = null;
-    document.getElementById("b3").innerHTML = '<form action="#" onsubmit="return delData()" name="nam"><label><select name="opts2" id="del"><option value="" selected>削除するワールドを以下から選択</option></select><br><button type="submit">削除</button></label></form><br>';
-    var dList = JSON.parse(localStorage.getItem("document_list"));
-    var ls = dList.splice(1, dList.length - 1);
-    for (var i = 0; i < ls.length; i++) {
-        document.getElementById("del").innerHTML += `<option value="` + ls[i] + `">` + ls[i] + `</option>`;
-    }
+    setDel();
+    setWorld();
     return false;
 }
 
@@ -179,20 +129,10 @@ function delData() {
     console.log(document.nam.elements[0].value);
     var delName = document.nam.elements[0].value;
     if (delName == "") {
-        document.getElementById("b1").innerHTML = "ワールドを削除します。zonest.cn/zが起動しなくなった場合はお使いください";
-        document.getElementById("b2").innerHTML = null;
-        document.getElementById("b4").innerHTML = null;
-        document.getElementById("b3").innerHTML = '<form action="#" onsubmit="return delData()" name="nam"><label><select name="opts2" id="del"><option value="" selected>削除するワールドを以下から選択</option></select><br><button type="submit">削除</button></label></form><br>';
-        var dList = JSON.parse(localStorage.getItem("document_list"));
-        var ls = dList.splice(1, dList.length - 1);
-        for (var i = 0; i < ls.length; i++) {
-            document.getElementById("del").innerHTML += `<option value="` + ls[i] + `">` + ls[i] + `</option>`;
-        }
+        setDel();
+        setWorld();
     } else {
-        document.getElementById("b1").innerHTML = "しばらくお待ちください...";
-        document.getElementById("b2").innerHTML = null;
-        document.getElementById("b3").innerHTML = null;
-        document.getElementById("b4").innerHTML = null;
+        plWait();
         var an = window.prompt("'" + delName + "'を削除します\nよろしければ'ok'と入力してください");
         if (an == "ok") {
             var n = JSON.parse(localStorage.getItem("document_list"));
@@ -204,15 +144,7 @@ function delData() {
         else {
             window.alert("削除されませんでした");
         }
-        document.getElementById("b1").innerHTML = '<p>以下から選んでください</p><form action="#" onsubmit="return getData()"><label><button type="submit">ダウンロードする</button></label></form><br>';
-        document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()" name="nam"><label><select name="opts" id="share"><option value="" selected>アップロードする--ワールドを以下から選択</option></select><br><button type="submit">決定</button></label></form><br>'
-        document.getElementById("b3").innerHTML = '<form action="http://zonest.cn/z/"><label><button type="submit" id="rev">元の画面に戻る</button></label></form>'
-        document.getElementById("b4").innerHTML = '<br><br><br><br><br><form action="#" onsubmit="return del()"><label><button type="submit">ワールドを強制削除する</button></label></form>'
-        var dList = JSON.parse(localStorage.getItem("document_list"));
-        var ls = dList.splice(1, dList.length - 1);
-        for (var i = 0; i < ls.length; i++) {
-            document.getElementById("share").innerHTML += `<option value="` + ls[i] + `">` + ls[i] + `</option>`;
-        }
+        setDefalut();
     }
     return false;
 }
