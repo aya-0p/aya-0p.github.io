6c85e1d7e9decb7514c3ae76d3f6f72a836cbb51
aya-0p <69342748+aya-0p@users.noreply.github.com>
2021-07-12 18:56:17 +0900

    Update share_beta.js

--- other/share_beta.js
+++ other/share_beta.js
@@ -1,9 +1,9 @@
-//developer ver 0.3.5
-console.log("developer ver 0.3.5");
+//developer ver 0.3.6
+console.log("developer ver 0.3.6");
 if (localStorage.getItem("document_list") == null) {
     localStorage.setItem("document_list", JSON.stringify(["we start"]));
 }
-(document.getElementsByTagName("body"))[0].innerHTML = '<div id="b1"></div><br><div id=b2></div><div id="b3"></div><div id="b35"></div><div id="b36"></div><div id="b4"></div><footer><div>Version 0.3.5</div><a href="javascript:(function(d,j,s)%7Bs=d.createElement(\'script\');s.src=j;d.body.appendChild(s);%7D)(document,\'https://aya-0p.github.io/other/share_beta.js\')">開発版を利用する</a><style>footer{position: absolute;bottom: 0;}</style></footer>';
+(document.getElementsByTagName("body"))[0].innerHTML = '<div id="b1"></div><br><div id=b2></div><div id="b3"></div><div id="b35"></div><div id="b36"></div><div id="b4"></div><footer><div>Version 0.3.6</div><a href="javascript:(function(d,j,s)%7Bs=d.createElement(\'script\');s.src=j;d.body.appendChild(s);%7D)(document,\'https://aya-0p.github.io/other/share_beta.js\')">開発版を利用する</a><style>footer{position: absolute;bottom: 0;}</style></footer>';
 function setDefault() {
     document.getElementById("b1").innerHTML = '<p>以下から選んでください</p><form action="#" onsubmit="return getData()"><label><button type="submit">ダウンロードする</button></label></form><br>';
     document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()" name="nam"><label><select name="opts" id="share"><option value="" selected>アップロードする--ワールドを以下から選択</option></select><br><br><button type="submit">決定</button></label></form><br>';
@@ -216,11 +216,12 @@ function addData() {
         }
     }
 }
-let input = document.getElementById('file');
+var input = document.getElementById('file');
+var reader = new FileReader();
 input.addEventListener('change', () => {
-　　　　for(file of input.files){
-           reader.readAsText(file, 'UTF-8');
-           reader.onload = ()=> {
+　　for(file of input.files){
+        reader.readAsText(file, 'UTF-8');
+        reader.onload = ()=> {
            ulData(reader.result);
         };
     }
