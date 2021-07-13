40a9eacf06482aa2709ea3e00e2e5738acbe3787
aya-0p <69342748+aya-0p@users.noreply.github.com>
2021-07-13 12:27:15 +0900

    Add files via upload

--- other/share_beta.js
+++ other/share_beta.js
@@ -1,9 +1,9 @@
-//developer ver 0.3.15
-console.log("developer ver 0.3.15");
+//developer ver 0.3.16
+console.log("developer ver 0.3.16");
 if (localStorage.getItem("document_list") == null) {
     localStorage.setItem("document_list", JSON.stringify(["we start"]));
 }
-(document.getElementsByTagName("body"))[0].innerHTML = '<div id="b1"></div><br><div id=b2></div><div id="b3"></div><div id="b35"></div><div id="b36"></div><div id="b4"></div><footer><div>Version 0.3.15</div><a href="javascript:(function(d,j,s)%7Bs=d.createElement(\'script\');s.src=j;d.body.appendChild(s);%7D)(document,\'https://aya-0p.github.io/other/share_beta.js\')">開発版を利用する</a><style>footer{position: absolute;bottom: 0;}</style></footer>';
+(document.getElementsByTagName("body"))[0].innerHTML = '<div id="b1"></div><br><div id=b2></div><div id="b3"></div><div id="b35"></div><div id="b36"></div><div id="b4"></div><footer><div>Version 0.3.16</div><a href="javascript:(function(d,j,s)%7Bs=d.createElement(\'script\');s.src=j;d.body.appendChild(s);%7D)(document,\'https://aya-0p.github.io/other/share_beta.js\')">開発版を利用する</a><style>footer{position: absolute;bottom: 0;}</style></footer>';
 function setDefault() {
     document.getElementById("b1").innerHTML = '<p>以下から選んでください</p><form action="#" onsubmit="return getData()"><label><button type="submit">ダウンロードする</button></label></form><br>';
     document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()" name="nam"><label><select name="opts" id="share"><option value="" selected>アップロードする--ワールドを以下から選択</option></select><br><br><button type="submit">決定</button></label></form><br>';
@@ -228,7 +228,7 @@ function addData() {
 }
 function ulData(e) {
     //console.log(JSON.parse(e));
-    if (isValidJson(e) == true) {
+    if (isValidJson(e) == false) {
         window.alert("ファイルが正しくないです");
         console.log("load error - not JSON");
     } else {
