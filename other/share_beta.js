8a51370876d927681906074b63ed5621b71cac23
aya-0p <69342748+aya-0p@users.noreply.github.com>
2021-07-13 10:10:09 +0900

    Add files via upload

--- other/share_beta.js
+++ other/share_beta.js
@@ -1,9 +1,9 @@
-//developer ver 0.3.12
-console.log("developer ver 0.3.12");
+//developer ver 0.3.13
+console.log("developer ver 0.3.13");
 if (localStorage.getItem("document_list") == null) {
     localStorage.setItem("document_list", JSON.stringify(["we start"]));
 }
-(document.getElementsByTagName("body"))[0].innerHTML = '<div id="b1"></div><br><div id=b2></div><div id="b3"></div><div id="b35"></div><div id="b36"></div><div id="b4"></div><footer><div>Version 0.3.12</div><a href="javascript:(function(d,j,s)%7Bs=d.createElement(\'script\');s.src=j;d.body.appendChild(s);%7D)(document,\'https://aya-0p.github.io/other/share_beta.js\')">開発版を利用する</a><style>footer{position: absolute;bottom: 0;}</style></footer>';
+(document.getElementsByTagName("body"))[0].innerHTML = '<div id="b1"></div><br><div id=b2></div><div id="b3"></div><div id="b35"></div><div id="b36"></div><div id="b4"></div><footer><div>Version 0.3.13</div><a href="javascript:(function(d,j,s)%7Bs=d.createElement(\'script\');s.src=j;d.body.appendChild(s);%7D)(document,\'https://aya-0p.github.io/other/share_beta.js\')">開発版を利用する</a><style>footer{position: absolute;bottom: 0;}</style></footer>';
 function setDefault() {
     document.getElementById("b1").innerHTML = '<p>以下から選んでください</p><form action="#" onsubmit="return getData()"><label><button type="submit">ダウンロードする</button></label></form><br>';
     document.getElementById("b2").innerHTML = '<form action="#" onsubmit="return postData()" name="nam"><label><select name="opts" id="share"><option value="" selected>アップロードする--ワールドを以下から選択</option></select><br><br><button type="submit">決定</button></label></form><br>';
@@ -228,19 +228,28 @@ function addData() {
 }
 function ulData(e) {
     //console.log(JSON.parse(e));
-    var datas = JSON.parse(e);
-    console.log(datas);
-    if (datas[0] != "backupDatasForZonestcn") {
+    if (isValidJson(value) == true) {
         window.alert("ファイルが正しくないです");
+        console.log("load error - not JSON");
     } else {
-        var datas2 = datas.splice(1, datas.length - 1);
-        var wns = ["we start"];
-        datas2.forEach(function(value, index) {
-            console.log(datas2[index][0]);
-            console.log(datas2[index][1]);
-            wns.push(datas2[index][0]);
-        });
-        console.log(wns);
+        var datas = JSON.parse(e);
+        console.log(datas);
+        if (datas[0] != "backupDatasForZonestcn") {
+            window.alert("ファイルが正しくないです");
+            console.log("load error - not found backupDatasForZonestcn");
+        } else {
+            var datas2 = datas.splice(1, datas.length - 1);
+            var wns = ["we start"];
+            localStorage.clear()
+            datas2.forEach(function(value, index) {
+                localStorage.setItem(datas2[index][0],datas2[index][1])
+                console.log(datas2[index][0]);
+                console.log(datas2[index][1]);
+                wns.push(datas2[index][0]);
+            });
+            localStorage.setItem("document_list",wns)
+            console.log(wns);
+        }
     }
     setDefault();
     setWorld();
@@ -261,4 +270,12 @@ function dlData() {
     link.download = 'worlds.json';
     link.click();
     return false;
+}
+isValidJson(value) {
+  try {
+    JSON.parse(value)
+  } catch (e) {
+    return false
+  }
+  return true
 }
